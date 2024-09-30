059325b14391fe57e08f1bbebc2fff99f1728cd8
kindirsurprise <127309377+kindirsurprise@users.noreply.github.com>
2024-09-30 21:31:45 +0400

    Update ProductList.jsx

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -329,7 +329,7 @@ const handlePlantsClick = (e) => {
                   />
                   <div className="product-title">{plant.name}</div>
                   <div className="product-description">{plant.description}</div>
-                  <div className="product-cost">{`${plant.cost}`}</div>
+                  <div className="product-cost">{{plant.cost}}</div>
                   <button
                     className="product-button"
                     onClick={() => handleAddToCart(plant)}
